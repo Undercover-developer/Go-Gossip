18a5852ffc7f812c92eaddd9669b19c02f6ea2d9
Koleoso oyindamola <87513130+Undercover-developer@users.noreply.github.com>
2025-02-20 13:20:25 -0600

    initial commit

---
+++ node.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Node struct {
+	ID      string
+	Address net.Addr
+	Peers   []net.Addr
+}
+
+func (n *Node) Start() {
+	listener, err := net.Listen(n.Address.Network(), n.Address.String())
+	if err != nil {
+		fmt.Printf("Node %s: error starting server on %s: %v\n", n.ID, n.Address, err)
+		return
+	}
+
+	fmt.Printf("Node %s: listening on %s\n", n.ID, n.Address)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Node %s: error accepting connection: %v\n", n.ID, err)
+			continue
+		}
+
+		go n.handleConnection(conn)
+	}
+}
+
+func (n *Node) handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	nBytes, err := conn.Read(buffer)
+
+	if err != nil {
+		fmt.Printf("Node %s: error reading data: %v\n", n.ID, err)
+		return
+	}
+
+	message := string(buffer[:nBytes])
+	fmt.Printf("Node %s: received message: %s\n", n.ID, message)
+}
